fcaf3b79dff33f4ca583ffccb76aa1d9760a5736
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-04 14:08:40 +0530

    Wether request successfull

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
- const request = require('request');
-// const yargs = require('yargs');
-// const geocode = require('./geocode/geocode');
+const request = require('request');
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
-// const argv = yargs
-//     .options({
-//     a: {
-//         demand: true,
-//         alisa : 'address',
-//         describe : 'Address to featch wether for',
-//         string: true
-//     }
-// })
-// .help()
-// .alias('help', 'h')
-// .argv;
-// geocode.geocodeAddress(argv.a ,(errorMessage, results) =>{
-//     if(errorMessage) {
-//         console.log(errorMessage);
-//     }
-//     else{
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
+const argv = yargs
+    .options({
+    a: {
+        demand: true,
+        alisa : 'address',
+        describe : 'Address to featch wether for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+geocode.geocodeAddress(argv.a ,(errorMessage, results) =>{
+    if(errorMessage) {
+        console.log(errorMessage);
+    }
+    else{
+        console.log(JSON.stringify(results, undefined, 2));
+    }
 
-// });
+});
 
-// //5c62dbd1f0372135faf6dfd0f60c9710
+//5c62dbd1f0372135faf6dfd0f60c9710
