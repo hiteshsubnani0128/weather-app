bfedb7a7d22daccbe44ab8ef4c6b51c9e0c8f2aa
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-04 12:41:05 +0530

    app changed

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 var formattedAddress = encodeURIComponent(argv.a);
+
 request({
     url : 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDMIBY1H3RBPhmjLRO6zmh3-jWe9eJZZsc&address='+ formattedAddress,
     json : true
@@ -24,7 +25,7 @@ request({
     else if(body.status === 'ZERO_RESULTS'){
         console.log('Unable to find that Address.');
     }
-    else{
+    else if(body.status === 'OK'){
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`lat: ${body.results[0].geometry.location.lat}`);
     console.log(`lat: ${body.results[0].geometry.location.lng}`);
