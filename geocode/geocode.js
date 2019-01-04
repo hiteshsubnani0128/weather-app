14ad9ae10240ee9294ce4c9433a2d1a30ee14585
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-04 13:32:24 +0530

    callback funcn added

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const geocodeAddress = (address) =>{
+const geocodeAddress = (address, callback) =>{
 
 var formattedAddress = encodeURIComponent(address);
 
@@ -9,15 +9,20 @@ request({
     json : true
 }, (error, response, body) => {
     if(error){
-        console.log('Unable to connect to Google server');
+        callback('Unable to connect to Google server');
     }
     else if(body.status === 'ZERO_RESULTS'){
-        console.log('Unable to find that Address.');
+        callback('Unable to find that Address.');
     }
     else if(body.status === 'OK'){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`lat: ${body.results[0].geometry.location.lng}`);
+        callback(undefined, {
+            address: body.results[0].formatted_address,
+            latitude:body.results[0].geometry.location.lat,
+            longitude : body.results[0].geometry.location.lat
+        });
+    //console.log(`Address: ${body.results[0].formatted_address}`);
+    //console.log(`lat: ${body.results[0].geometry.location.lat}`);
+    //console.log(`lat: ${body.results[0].geometry.location.lng}`);
  } });
 
 }
